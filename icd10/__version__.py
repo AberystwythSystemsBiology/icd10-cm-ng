28d7f9de5302a2451236105ad5f02df2f070b77d
Keiron O'Shea <keiron.oshea@gmail.com>
2022-02-01 11:38:32 +0000

    Update __version__.py

--- icd10/__version__.py
+++ icd10/__version__.py
@@ -3,7 +3,7 @@ __description__ = ('ICD-10 codes for diseases, signs and symptoms, abnormal find
                    'complaints, social circumstances, and external causes of injury or disease')
 __url__ = 'https://github.com/AberystwythSystemsBiology/icd10-cm-ng'
 __version__ = '0.0.5'
-__author__ = 'Keiron O\'Shea'
+__author__ = 'Keiron OShea'
 __author_email__ = 'keiron@fastmail.co.uk'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2022 Keiron O\'Shea'
